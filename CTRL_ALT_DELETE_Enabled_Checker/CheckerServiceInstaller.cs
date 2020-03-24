7c68d47908ab222556a00a1f5b3c7dea9ce3bdf9
Alexandr Kutiev <antizboy1985@gmail.com>
2020-03-24 11:19:02 +0300

    Add installer to this Service project

---
+++ CTRL_ALT_DELETE_Enabled_Checker/CheckerServiceInstaller.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ServiceProcess;
+
+
+namespace CTRL_ALT_DELETE_Enabled_Checker
+{
+    [RunInstaller(true)]
+    public partial class CheckerServiceInstaller : System.Configuration.Install.Installer
+    {
+        ServiceInstaller serviceInstaller;
+        ServiceProcessInstaller processInstaller;
+
+        public CheckerServiceInstaller()
+        {
+            InitializeComponent();
+
+            serviceInstaller = new ServiceInstaller();
+            processInstaller = new ServiceProcessInstaller();
+
+            processInstaller.Account = ServiceAccount.LocalSystem;
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+            serviceInstaller.ServiceName = "CTRL_ALT_DELETE_Enabled_Checker";
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
